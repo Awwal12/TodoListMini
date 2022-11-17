c5439a5fe78e89b9bc04ba27dd5c187a40f15770
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2022-11-17 15:38:29 +0100

    added basic functionality

---
+++ assets/js/todos.js
@@ -0,0 +1,31 @@
+var checked = false;
+// checking off the marked item
+$("ul").on("click","li",function(){
+ if (!checked) {
+  $(this).css("color", "grey",);
+ $(this).css("text-decoration", "line-through");
+ } else {
+  $(this).css("color", "black",);
+ $(this).css("text-decoration", "none");
+ }
+
+ checked = !checked;
+});
+//click on x to delete
+$("ul").on("click","span", function(event){
+ $(this).parent().fadeOut(500,function(){
+  $(this).remove()
+ });
+ event.stopPropagation();
+});
+//creating a new todo
+$("#txt").keypress(function(event){
+if(event.which === 13){
+//grab the value of the new todo
+var newText = $(this).val(); 
+//reset the val
+$(this).val("");
+//create a new li and add to ul
+$("ul").append("<li><span>X</span> " + newText + "</li>");  
+};
+});
